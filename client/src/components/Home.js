7ed91664edc83e197f8a933eafd557be5a093d56
Eugene Penaranda <eugenepenaranda007@gmail.com>
2017-09-02 06:08:41 -0700

    Style Home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,9 +4,15 @@ import { RaisedButton } from 'material-ui';
 
 const Home = () => (
   <div className="home-container-div">
-    <Link to="/form1">
-      <RaisedButton label="Start Onboard Process" />
-    </Link>
+    <div className="home-inner-container-div">
+      <div className="home-details-div">
+        <h1>EKA SOLUTIONS</h1>
+        <p>Start onboarding process by clicking Start button.</p>
+        <Link to="/form1" >
+          <RaisedButton label="Start" primary={true} />
+        </Link>
+      </div>
+    </div>
   </div>
 );
 
